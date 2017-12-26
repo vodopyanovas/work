0b6274dadb98f5b2fef6206ee80137d24d01c92d
Vodopyanov Anton <vodopyanovas@ya.ru>
2017-12-26 16:53:39 +0300

    new functions init

--- parser.py
+++ parser.py
@@ -26,8 +26,7 @@ from paramiko.ssh_exception import AuthenticationException
 
 # скачивает файл логов с указанного сервера по ssh
 def download_logfile(settings, srv):
-    # лог ssh подключения
-    # paramiko.util.log_to_file('paramiko_lib.log')
+    # paramiko.util.log_to_file('paramiko_lib.log')  # включение логирования ssh подключения
 
     log_time = int(now) - 1
 
@@ -42,7 +41,7 @@ def download_logfile(settings, srv):
     )
 
     sftp = ssh.open_sftp()
-    remote_path = '/app/fusion/wire_gate/log/'
+    remote_path = '/app/fusion/wire_gate/log/'  # путь к логам на сервере
 
     # маска для поиска файлов вида support_log.20170927185959998.gz
     pattern = re.compile(r"support_log." + str(log_time) + "\d{7}.gz")
@@ -88,24 +87,38 @@ def parser(message):
     result[0] = timestamp
     result[1] = sess_id
     result.insert(2, tw_login)
+    msg_type = result[3]
 
-    return result
+    return result, msg_type
 
 
 def file_handling():
     pass
 
 
+def write_to_db(object):
+    pass
+    # new_ord_req = NewOrderSingle(
+    #     server_id=server_id, timestamp=timestamp, sess_id=sess_id, tw_login=tw_login,
+    #     msg_type=msg_type, security_id=security_id, price=price, order_qty=order_qty,
+    #     cl_ord_id=cl_ord_id, cl_ord_link_ID=cl_ord_link_ID, side=side, account=account,
+    #     expire_date=expire_date, time_in_force=time_in_force
+    # )
+
+    # session.add(<экземпляр класса>)
+    # session.commit()
+
+
 if __name__ == "__main__":
     # while True:
     try:
         print('Скачиваю файлы')
-        file_name = download_logfile(tw_srv1, 1)
-        print('Файл скачан:', file_name)
-        file_name = download_logfile(tw_srv2, 2)
-        print('Файл скачан:', file_name)
+        file_name1 = download_logfile(tw_srv1, 1)
+        print('Файл скачан:', file_name1)
+        file_name2 = download_logfile(tw_srv2, 2)
+        print('Файл скачан:', file_name2)
         print('Создаю БД')
-        create_db()
+        session = create_db()
         print('БД создана: ' + now + '.sqlite')
 
     except KeyboardInterrupt:
@@ -115,3 +128,9 @@ if __name__ == "__main__":
         print('[Error] Authentication error! Check login/password')
     except Exception as e:
         print('[Error]', e)
+
+
+# TODO:
+1. Сделать логирование
+2. Работа с файлами логов: открыть нужный файл, циклом идти по строкам и парсить их
+3. Допилить запись в БД
